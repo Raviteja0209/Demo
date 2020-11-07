484f1649ae390357a9b4053e7277f978ef90537b
raviteja <raviteja@vizag.com>
2020-11-07 16:41:34 +0530

    recoded

---
+++ src/gitdemo/logout.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author win10
+ */
+public class logout {
+    public void logoutSetup(){
+        System.out.println("Logout successfully");
+    }
+}
