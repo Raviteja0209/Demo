76429d92725dd41b8575c7b201be7d80794f6871
raviteja <raviteja@vizag.com>
2020-11-07 16:37:13 +0530

    first code to commit

---
+++ src/gitdemo/Gitdemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author win10
+ */
+public class Gitdemo {
+
+  
+    public static void main(String[] args) {
+       hello(); 
+    }
+    public static void hello(){
+        System.out.println("HI MURALI KRISHNA");
+    }
+    
+}
---
+++ src/gitdemo/login.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author win10
+ */
+public class login {
+    public void loginSetup(){
+        System.out.println("Login-setup");
+    }
+    
+}
